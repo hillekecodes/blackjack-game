d3cab9d0dd4cc466fdb0f83581910e00b6924597
David <mr.hilleke@gmail.com>
2022-07-20 22:40:56 -0400

    got points from cards and a few other things I'm sure I forgot

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ const makeDeck = (rank, suit) => {
   const card = {
     rank: rank,
     suit: suit,
-    pointValue: rank > 10 ? 10 : rank,
+    pointValue: rank.length > 2 ? 10 : rank === 10? 10 : rank
   };
   deck.push(card);
 };
@@ -18,6 +18,64 @@ for (let suit of suits) {
   }
 }
 
+const dealButton = document.querySelector('#deal-button');
+const hitMeButton = document.querySelector('#hit-button');
+let dealerScore = [];
+let playerScore = [];
+
+//event listeners
 window.addEventListener("DOMContentLoaded", () => {
   // Execute after page load
 });
+dealButton.addEventListener('click', deal);
+hitMeButton.addEventListener('click', hitMe);
+
+
+//functions
+function shuffleDeck(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function deal (event) {
+  let counter = 2;
+  while(counter > 0)
+  if(event.target.id === 'deal-button'){
+    shuffleDeck(deck);
+    const dealerCard = document.createElement('img'); 
+    const dealerCardInfo = deck.pop();
+    const dealerPoints = dealerCardInfo.pointValue;
+    dealerScore.push(dealerPoints);
+    dealerCard.setAttribute('src', `./images/${dealerCardInfo.rank}_of_${dealerCardInfo.suit}.png`);
+    dealerHand.appendChild(dealerCard);
+    const playerCard = document.createElement('img'); 
+    const playerCardInfo = deck.pop();
+    const playerPoints = playerCardInfo.pointValue;
+    playerScore.push(playerPoints);
+    playerCard.setAttribute('src', `./images/${playerCardInfo.rank}_of_${playerCardInfo.suit}.png`);
+    playerHand.appendChild(playerCard);
+    counter --;
+  }
+}
+
+function hitMe (event) {
+  if(event.target.id === 'hit-button'){
+    const dealerCard = document.createElement('img'); 
+    const dealerCardInfo = deck.pop();
+    const dealerPoints = dealerCardInfo.pointValue;
+    dealerScore.push(dealerPoints);
+    dealerCard.setAttribute('src', `./images/${dealerCardInfo.rank}_of_${dealerCardInfo.suit}.png`);
+    dealerHand.appendChild(dealerCard);
+    const playerCard = document.createElement('img'); 
+    const playerCardInfo = deck.pop();
+    const playerPoints = playerCardInfo.pointValue;
+    playerScore.push(playerPoints);
+    playerCard.setAttribute('src', `./images/${playerCardInfo.rank}_of_${playerCardInfo.suit}.png`);
+    playerHand.appendChild(playerCard);
+  }
+}
+
+
+// let cardInfo = deck[Math.floor(Math.random()) * deck.length]
